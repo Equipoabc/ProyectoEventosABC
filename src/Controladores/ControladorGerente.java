2d9b3d07c9d164da5d2c01cb10f9f2ffea66da81
Iván Toro <ivanmtoroc@gmail.com>
2017-10-22 15:42:15 -0500

    Project Scorpion

--- src/Controladores/ControladorGerente.java
+++ src/Controladores/ControladorGerente.java
@@ -9,6 +9,11 @@ package Controladores;
  *
  * @author Luis
  */
+
+import DAO.*;
+import Logica.*;
+
 public class ControladorGerente {
     
+     
 }
--- src/DAO/DaoGerente.java
+++ src/DAO/DaoGerente.java
@@ -9,6 +9,54 @@ package DAO;
  *
  * @author Luis
  */
+
+import java.sql.*;
+import Logica.*;
+import Conexion.*;
+
 public class DaoGerente {
+   
+    Conexiones conexion;
     
+    public DaoGerente(){
+        
+        conexion = new Conexiones();
+    }
+    
+    public Gerente loginGerenete(){
+        
+        String sql;
+        Gerente gerente = new Gerente();
+
+        sql = "SELECT cedula_ge, contrasena FROM Gerente";
+        
+        try{
+            Connection con = conexion.getConnetion();
+            Statement sentencia = con.createStatement();
+            ResultSet consulta = sentencia.executeQuery(sql);
+            
+            while(consulta.next()){
+                
+                gerente.setCedula_ge(consulta.getString(1));
+                gerente.setContrasena(consulta.getString(2));
+            }
+            
+            return gerente;
+        }
+        catch(SQLException e){
+            
+            System.out.println("Error: " + e); 
+        }
+        catch(Exception e){ 
+            
+            System.out.println("Error: " + e);
+        }
+        
+        return null;
+    }
+
+    public void cerrarConexionBD(){
+        
+        conexion.closeConection(conexion.getConnetion());
+    }
 }
